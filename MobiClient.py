db16c2ada74d0fd045c5ee3fcb3c246f15eaf853
Dario Salvi <dariosalvi78@gmail.com>
2021-05-14 14:54:43 +0200

    sending timestamps as millis not micros

--- MobiClient.py
+++ MobiClient.py
@@ -11,9 +11,10 @@ import struct
 
 timeBegin = datetime.datetime.now()
 
+
 def logIn(ema, passw):
-# Loging in with receieved userName and password
-# Returns a boolean about the succeed of the login process
+    # Loging in with receieved userName and password
+    # Returns a boolean about the succeed of the login process
     try:
         loginPost = requests.post('http://localhost:3000/api/login', json={
             'email': ema,
@@ -36,31 +37,32 @@ def logIn(ema, passw):
     except:
         return False
 
+
 def sendEvent(eve, det):
-# Sends event from the task instruction to the server as a json file
+    # Sends event from the task instruction to the server as a json file
     global ws
     global timeBegin
     # timedelta = (datetime.datetime.now())
     timedelta = (datetime.datetime.now() - timeBegin)
     x = {
-        "ts": math.floor(timedelta.total_seconds()*1000000),
-        #Time as time if Dario wnats it
-        # "ts": str(timedelta.hour) +":" + str(timedelta.minute) + ":" + str(timedelta.second),
+        # time is in milliseconds since start
+        "ts": math.floor(timedelta.total_seconds() * 1000),
         "events": eve,
         "details": det
-        }
+    }
     ws.send(json.dumps(x))
 
+
 def sendDataToServer(ts, fc3, fcz, fc4, c3, cz, c4, cp3, cpz, cp4):
-# Receives data from each channel and sends them to the server as a json file
+    # Receives data from each channel and sends them to the server as a json file
 
     # timedelta = datetime.datetime.now()
     # global end
     global timeBegin
 
     timedelta = (ts - timeBegin)
-    bytes = (math.floor(timedelta.total_seconds() * 1000000)
-        ).to_bytes(4, byteorder='little', signed=False)
+    bytes = (math.floor(timedelta.total_seconds() * 1000)  # time is in milliseconds since start
+             ).to_bytes(4, byteorder='little', signed=False)
     # bytes = (datetime.datetime.now()).to_bytes(4, byteorder='little', signed=False)
     # then we send 9 samples of unsigned 32 bits integers
 
@@ -76,15 +78,19 @@ def sendDataToServer(ts, fc3, fcz, fc4, c3, cz, c4, cp3, cpz, cp4):
 
     ws.send_binary(bytes)
 
+
 def setTime():
     global timeBegin
     timeBegin = datetime.datetime.now()
 
+
 def closeConnection():
     global ws
     ws.close()
+
+
 '''
 john.doe@test.test
 outerZpace
 '''
-#Done
+# Done
